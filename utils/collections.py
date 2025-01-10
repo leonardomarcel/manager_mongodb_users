c13272658f2069c46e1ec28f7f13607d5ebe0c69
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-01-10 14:17:03 -0300

    Refactor: passing a MongoDb client through the environment variable

--- utils/collections.py
+++ utils/collections.py
@@ -1,11 +1,11 @@
 import pymongo
-from bson import ObjectId
+import os
 
 class MongoClient:
+    MONGO_URI = os.environ['MONGO_URI']
     def __init__(self, bd_name, collection_name):
-        self.client = pymongo.MongoClient("mongodb://200.100.50.50:27017/")
+        self.client = pymongo.MongoClient(self.MONGO_URI)
         self.db = self.client[bd_name]  # Nome do banco de dados
         self.collection = self.db[collection_name]  # Nome da coleção
     def get_collection(self):
         return self.collection
-    
