290ce74b8c4c0a4a11d438ea6001b85c8591ede0
leonardomarcel <leonardo.carvalho@ecoit.com.br>
2025-01-10 14:03:34 -0300

    Add: implementing a wrapper function token_required for decode jwt tokens sended on protected apis

--- decoratores/decoratores.py
+++ decoratores/decoratores.py
@@ -1,10 +1,12 @@
 from functools import wraps
-
+import jwt
+import os
 from flask import (Flask,
                    redirect,
                    render_template_string,
                    request,
                    session,
+                   jsonify,
                    url_for)
 
 app = Flask(__name__)
@@ -13,8 +15,28 @@ app = Flask(__name__)
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        
         if session.get("auth") is None:
             return redirect(url_for("users.login", next=request.url))
         return f(*args, **kwargs)
 
     return decorated_function
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get("Authorization").split()[1]
+        try:
+            jwt.decode(token, os.environ["SECRET_KEY"], algorithms=["HS256"])
+        except Exception as e:
+            return jsonify({
+                "NOK": "Token is invalid or expired",
+                "message": str(e),
+                "token": token
+            }), 401
+            
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+    
